8800a52c76ae94bc7d2d526768439afa19260f34
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-02-14 21:16:36 +0900

    [#7] Feat: 메뉴버튼 온오프 개발

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,42 @@
 import React, { useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { AiOutlineClose } from 'react-icons/ai';
+
 type Props = {};
 
-const GnbMenu = () => {
-  return (
-    <div id='gnbMenu' className='w-full h-screen bg--black-100 z-[50]'>
-      <div className='flex flex-col w-full mx-auto px-4'>
-        <div className='flex flex-col space-y-2 text-gray-500'>
-          <a className='hover:underline menu-item-root' href='/1'>
-            메뉴 1
-          </a>
-          <a className='hover:underline menu-item-root' href='/2'>
-            메뉴 2
-          </a>
-          <a className='hover:underline menu-item-root' href='/3'>
-            메뉴 3
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
 const Header = (props: Props) => {
   const [isOpen, setisOpen] = useState(false);
-  function toggleMenu() {
+  const toggleMenu = () => {
     setisOpen((isOpen) => !isOpen);
-  }
-  return (
-    <div className='flex relative m-auto top-8 h-12 w-96 '>
-      <div className='absolute inset-x-0 top-0 text-center text-lg'>
-        <span className='text-mw font-semibold'>MW </span>
-        <span className='--black-100 font-light'>Bank</span>
+  };
+  if (!isOpen) {
+    return (
+      <div className='flex relative m-auto top-8 h-12 w-96 '>
+        <div className='absolute inset-x-0 top-0 text-center text-lg'>
+          <span className='text-mw font-semibold'>MW </span>
+          <span className='--black-100 font-light'>Bank</span>
+        </div>
+        <button className='absolute right-2 top-1' onClick={() => toggleMenu()}>
+          <AiOutlineMenu size='28' />
+        </button>
       </div>
-      <button className='absolute right-2 top-1' onClick={() => toggleMenu()}>
-        <AiOutlineMenu size='28' />
-      </button>
-      {isOpen ? <GnbMenu /> : null}
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div id='gnbMenu' className='w-full h-screen bg-[#000] z-[50]'>
+        <div id='menuHeader' className='w-full h-[210px] bg-mw'>
+          <div
+            className='float-right mt-9 mr-[50px]'
+            onClick={() => toggleMenu()}
+          >
+            {' '}
+            <AiOutlineClose size='32' color='#fff' />
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Header;
--- src/components/MainPage/GoodsList.tsx
+++ src/components/MainPage/GoodsList.tsx
@@ -29,7 +29,7 @@ const lists = {};
 const GoodsList = (props: props) => {
   return (
     <div className='relative'>
-      <div className='box-border mt-20 font-semibold text-xl p-10'>
+      <div className='box-border mt-20 ml-3 font-semibold text-xl p-10'>
         <button>
           <Link to='/' className='flex text-center align-middle'>
             <span>전체 상품</span>
